6408167364a875926900dca2613bc010931080f8
Dhiraj Kahar <dhirajkahar7@gmail.com>
2022-12-11 23:39:11 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -4,42 +4,42 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('home', {
-    url:`http://localhost:3000/`
+    url:process.env.HOST_URL
    })
 });
 
 /* GET contact-us. */
 router.get('/contact-us', function(req, res, next) {
   res.render('contact', {
-    url:`http://localhost:3000/`
+    url:process.env.HOST_URL
    })
 });
 
 /* GET home page. */
 router.get('/faq', function(req, res, next) {
   res.render('faq', {
-    url:`http://localhost:3000/`
+    url:process.env.HOST_URL
    })
 });
 
 /* GET home page. */
 router.get('/confirm', function(req, res, next) {
   return res.render('confirm', {
-    url:`http://localhost:3000/`
+    url:process.env.HOST_URL
    })
 });
 
 /* GET home page. */
 router.get('/error', function(req, res, next) {
   return res.render('error', {
-    url:`http://localhost:3000/`
+    url:process.env.HOST_URL
    })
 });
 
 /* GET home page. */
 router.get('/user-present', function(req, res, next) {
   return res.render('user-present', {
-    url:`http://localhost:3000/`
+    url:process.env.HOST_URL
    })
 });
 
