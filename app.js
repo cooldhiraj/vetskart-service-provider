022ebc5d295ab4ee2de1c7777d5dff3ff11b87d0
Dhiraj Kahar <dhirajkahar7@gmail.com>
2022-12-17 21:33:23 +0530

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
